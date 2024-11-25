fa1b3b2196aa17a3f284f9e3266146658934da2f
Tassochaves <tassogt@hotmail.com>
2024-11-25 17:56:11 -0300

    Feat: add UsuarioController e implementa o retornaUsuarioPorId, criarUsuario

---
+++ src/main/java/com/dev/api_loja/controller/UsuarioController.java
@@ -0,0 +1,52 @@
+package com.dev.api_loja.controller;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.api_loja.excecoes.RecursoNaoEncontradoExcecao;
+import com.dev.api_loja.excecoes.UsuarioExistenteExcecao;
+import com.dev.api_loja.model.Usuario;
+import com.dev.api_loja.requisicao.AddUsuarioRequest;
+import com.dev.api_loja.resposta.ApiResponse;
+import com.dev.api_loja.service.usuario.IUsuarioService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/usuarios")
+public class UsuarioController {
+
+    private final IUsuarioService usuarioService;
+
+    @GetMapping("/{usuarioId}/usuario")
+    public ResponseEntity<ApiResponse> retornaUsuarioPorId(@PathVariable Long usuarioId) {
+
+        try {
+
+            Usuario usuario = usuarioService.retornaUsuarioPorId(usuarioId);
+            return ResponseEntity.ok(new ApiResponse("Sucesso!", usuario));
+        } catch (RecursoNaoEncontradoExcecao e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<ApiResponse> criarUsuario(@RequestBody AddUsuarioRequest usuarioRequest) {
+
+        try {
+            Usuario usuario = usuarioService.criaUsuario(usuarioRequest);
+            return ResponseEntity.ok(new ApiResponse("Usuario criado com sucesso!", usuario));
+        } catch (UsuarioExistenteExcecao e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
